d89a2716a73e3bbe3b7bceccdc119f03878bd92e
ayasamind <masayaliketennis@yahoo.co.jp>
2021-05-15 20:53:57 +0900

    回答を削除

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -5,21 +5,6 @@ async function getData() {
 }
 
 function test() {
-  return new Promise((r) =>
-    setTimeout(
-      () =>
-        r([
-          {
-            id: 1,
-            full_name: '大木 優',
-          },
-          {
-            id: 2,
-            full_name: '山田 太郎',
-          },
-        ]),
-      3000
-    )
-  )
+  return
 }
 
