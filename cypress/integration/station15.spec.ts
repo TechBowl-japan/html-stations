0cd9b42245b2a95123ae9500a55d96278e70b792
SuguruOoki <oki.work0527@gmail.com>
2021-06-01 20:14:34 +0900

    Add wait function for timer

--- cypress/integration/station15.spec.ts
+++ cypress/integration/station15.spec.ts
@@ -16,6 +16,8 @@ describe('Station15', () => {
 
   it('3秒後フルネームのリストが表示される', () => {
     cy.tick(3000)
+    cy.wait(0)
+
     cy.get('#result').then((result) => {
       expect(result.children()[0].innerHTML).eq("大木 優")
       expect(result.children()[1].innerHTML).eq("山田 太郎")
