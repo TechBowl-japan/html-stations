a0e158b2d1724d473364e2681a36ecda0d5f7150
toshi-bp <yttoshi-bp@outlook.jp>
2021-08-23 13:09:59 +0900

    station14

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -13,6 +13,8 @@ function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+    data.full_name = data.family_name + " " + data.first_name;
+    return data;
 }
 
 // 出力が想定される答え
