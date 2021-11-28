97822bd82ba3f408013be19495892bae2232027d
SN1994 <night-carnival1219@docomo.ne.jp>
2021-11-28 11:51:27 +0900

    station15 finish

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,16 +1,24 @@
 async function getData() {
-  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
-  // ３秒後にフルネームのリストを表示されるようにしましょう。
-  // 最初から表示されていると、クリアになりません。
-  const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
-  ];
-  const result = await test()
-  return await result
+    // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
+    // ３秒後にフルネームのリストを表示されるようにしましょう。
+    // 最初から表示されていると、クリアになりません。
+    const userList = [
+        { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
+        { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+    ];
+    const result = await test(userList)
+    return await result
 }
 
-function test() {
-  return
-}
+function test(list) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            list.map((listNama) => {
+                var fullName = listNama.family_name + ' ' + listNama.first_name;
+                listNama["full_name"] = fullName;
+            })
+            resolve(list);
+        }, 3000);
+    });
 
+}
