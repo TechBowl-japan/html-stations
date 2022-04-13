6e1e113cc05816c71727af60f5b33f4f4ce3f15f
Azu0925 <yamaneko564783@gmail.com>
2022-04-13 11:33:00 +0900

    station17新規作成

---
+++ cypress/integration/station17.spec.ts
@@ -0,0 +1,54 @@
+/**
+ * # 「Gridlayoutを使えるようになろう！」 - Gridlayout
+ */
+
+describe('Station17', () => {
+  beforeEach(() => {
+    cy.clock()
+    cy.visit('/station17.html')
+  })
+
+  it('レイアウトがGridlayoutになっている', () => {
+    cy.get('body').should('have.css', 'display', 'grid')
+  })
+
+  it('ヘッダーが指定の位置にある', () => {
+    cy.get('header')
+      .should('have.css', 'grid-row', '1 / 2')
+      .and('have.css', 'grid-column', '1 / 3')
+  })
+
+  it('フッターが指定の位置にある', () => {
+    cy.get('footer')
+      .should('have.css', 'grid-row', '5 / 6')
+      .and('have.css', 'grid-column', '1 / 3')
+  })
+
+  it('アイテム1が指定の位置にある', () => {
+    cy.get('div')
+      .contains('アイテム1')
+      .should('have.css', 'grid-row', '2 / 5')
+      .and('have.css', 'grid-column', '1 / 2')
+  })
+
+  it('アイテム2が指定の位置にある', () => {
+    cy.get('div')
+      .contains('アイテム2')
+      .should('have.css', 'grid-row', '2 / 3')
+      .and('have.css', 'grid-column', '2 / 3')
+  })
+
+  it('アイテム3が指定の位置にある', () => {
+    cy.get('div')
+      .contains('アイテム3')
+      .should('have.css', 'grid-row', '3 / 4')
+      .and('have.css', 'grid-column', '2 / 3')
+  })
+
+  it('アイテム4が指定の位置にある', () => {
+    cy.get('div')
+      .contains('アイテム4')
+      .should('have.css', 'grid-row', '4 / 5')
+      .and('have.css', 'grid-column', '2 / 3')
+  })
+})
