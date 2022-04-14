101e31c0eb973621b96dcb28e02895bb07de537f
Azu0925 <yamaneko564783@gmail.com>
2022-04-14 16:02:15 +0900

    station12から23への移行とstation12の新規作成

--- cypress/integration/station12.spec.ts
+++ cypress/integration/station12.spec.ts
@@ -1,5 +1,5 @@
 /**
- * # 「モジュールのインポート/エクスポートを使えるようになろう！」 - Import/Export Statement
+ * # 「要素の重なりを扱えるようになろう！」 - position layout & other property
  */
 
 describe('Station12', () => {
@@ -7,9 +7,15 @@ describe('Station12', () => {
     cy.visit('/station12.html')
   })
 
-  it('クリアの文言が表示される', () => {
-    cy.get('#result').then((result) => {
-      expect(result[0].innerHTML).eq("Station12をクリア!")
+  it('赤色の要素が青色の要素より上になっている', () => {
+    cy.get('#red').then((red) => {
+      expect(Number(red.css('z-index'))).to.be.greaterThan(0)
+    })
+  })
+
+  it('赤色の要素が透過されている', () => {
+    cy.get('#red').then((red) => {
+      expect(Number(red.css('opacity'))).to.be.lessThan(1)
     })
   })
 })
--- src/scripts/station12.js
+++
@@ -1,3 +0,0 @@
-function getData() {
-    return "Station12をクリア!"
-}
