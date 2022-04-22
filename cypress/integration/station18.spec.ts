8460d665fa0e90a95b15a0ff7c71528f2075802a
Àżu <53806909+Azu0925@users.noreply.github.com>
2022-04-22 15:04:37 +0900

    Feature/new station 18 (#95)
    * station18新規作成
    
    * station18 テスト修正
    
    * テスト修正

---
+++ cypress/integration/station18.spec.ts
@@ -0,0 +1,24 @@
+/**
+ * # 「CSSアニメーションを作ろう！」 - CSS Animation
+ */
+
+describe('Station18', () => {
+  beforeEach(() => {
+    cy.visit('/station18.html')
+  })
+
+  it('開いたときにアニメーションが走る', () => {
+    cy.get('#animation').then((animation) => {
+      const duration = Number(animation.css('animation-duration').slice(0, -1))
+      expect(duration).to.be.greaterThan(0)
+      expect(animation.css('animation-name')).not.to.be.eq('none')
+    })
+  })
+
+  it('アニメーションが終わった後も要素が表示されている', () => {
+    cy.get('#animation').then((animation) => {
+      expect(animation.css('animation-fill-mode')).not.to.be.eq('none')
+      expect(animation.css('animation-fill-mode')).not.to.be.eq('backwards')
+    })
+  })
+})
