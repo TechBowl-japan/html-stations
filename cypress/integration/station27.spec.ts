225f026c9436db960468a6ff26c4a5499c3c9436
Àżu <53806909+Azu0925@users.noreply.github.com>
2022-05-12 15:20:51 +0900

    station27新規追加 (#98)
    * station27新規追加
    
    * 問題修正
    
    * テスト修正
    
    * レビューを受けて修正
    
    * Update src/scripts/station27-common.js
    
    Co-authored-by: Hikaru Terazono <3c1u@tohkani.com>
    
    Co-authored-by: Hikaru Terazono <3c1u@tohkani.com>

---
+++ cypress/integration/station27.spec.ts
@@ -0,0 +1,32 @@
+/**
+ * # 「if文を使えるようになろう！」 - if
+ */
+
+import { compareColor } from '../utils/compareColor'
+
+describe('station27', () => {
+  beforeEach(() => {
+    cy.visit('/station27.html')
+  })
+
+  it('トグルボタンがONのときテキストの背景色が赤色になる', () => {
+    cy.get('#check')
+      .check({ force: true })
+      .then(() => {
+        cy.get('p#text').then((text) => {
+          expect(compareColor(text.css('background-color'), '#ff0000')).to.be
+            .true
+        })
+      })
+  })
+
+  it('トグルボタンがOFFのときテキストの背景色が無色になる', () => {
+    cy.get('#check')
+      .uncheck({ force: true })
+      .then(() => {
+        cy.get('p#text').then((text) => {
+          expect(compareColor(text.css('background-color'), '')).to.be.true
+        })
+      })
+  })
+})
