2f9f35a5baaad23ea9ba2fcc83dfce0f7c13c887
Azu0925 <yamaneko564783@gmail.com>
2022-05-12 16:38:10 +0900

    レビューを受けて修正

--- cypress/integration/station19.spec.ts
+++ cypress/integration/station19.spec.ts
@@ -11,7 +11,12 @@ describe('Station19', () => {
 
   it('svgのfillに#008000が適用されている', () => {
     cy.get('svg').then((svg) => {
-      expect(compareColor(svg.css('fill'), '#008000')).to.be.true
+      cy.get('rect').then((rect) => {
+        expect(
+          compareColor(svg.css('fill'), '#008000') ||
+            compareColor(rect.css('fill'), '#008000')
+        ).to.be.true
+      })
     })
   })
 })
