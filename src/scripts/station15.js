73cdda8686a3eeeb9431c2d555638d4d8a97006d
ryota misumi <3delta7@gmail.com>
2022-12-14 11:24:19 +0900

    fix typo

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,8 +3,8 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
+    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
   const result = await test()
   return await result
