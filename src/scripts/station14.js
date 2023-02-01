2cb7c39f8dc5bc5482cd47034c582fc725c57eb4
Ryota Misumi <deltama37@users.noreply.github.com>
2023-02-01 19:46:32 +0900

    fix typo (#108)

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -3,8 +3,8 @@ function getData() {
     // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
     // そのため、JavaScriptで連結させて生成することにしました。
     const test = [
-        { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-        { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+        { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
+        { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
     ];
     return test.map(buildFullName)
 }
@@ -17,6 +17,6 @@ function buildFullName(data) {
 
 // 出力が想定される答え
 // const test = [
-//     {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affilication: 'TechTrain', is_student: false},
-//     {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affilication: 'HogeHoge大学', is_student: true}
+//     {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affiliation: 'TechTrain', is_student: false},
+//     {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affiliation: 'HogeHoge大学', is_student: true}
 // ];
