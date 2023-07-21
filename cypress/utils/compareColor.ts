9cf48011c6950e0385b1e3ada439ffd08d6152e2
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-07-21 16:25:04 +0900

    update: rewrite station3

--- cypress/utils/compareColor.ts
+++ cypress/utils/compareColor.ts
@@ -1,22 +1,27 @@
+const kRegexColorHex = /^#([0-9a-f]{3}|[0-9a-f]{6})$/
+const kRegexColorRgb = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/
+const kRegexColorRgba = /^rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)$/
+
+const parseColor = (color: string): number => {
+  if (kRegexColorHex.test(color)) {
+    return parseInt(color.slice(1), 16)
+  } else if (kRegexColorRgb.test(color)) {
+    const [, r, g, b] = color.match(kRegexColorRgb)!
+
+    return (parseInt(r) << 16) | (parseInt(g) << 8) | parseInt(b)
+  } else if (kRegexColorRgba.test(color)) {
+    const [, r, g, b] = color.match(kRegexColorRgba)!
+    return (parseInt(r) << 16) | (parseInt(g) << 8) | parseInt(b)
+  }
+}
+
 /**
  * Compares two colors. Requires DOM.
- * 
+ *
  * @param a A color.
  * @param b Another color.
  * @returns True when the `a` and `b` is identical.
  */
 export const compareColor = (a: string, b: string): boolean => {
-  const elm = document.createElement('div')
-  elm.style.color = a
-  elm.style.backgroundColor = b
-  elm.style.display = 'none'
-
-  document.body.appendChild(elm)
-
-  const computedStyle = window.getComputedStyle(elm)
-  const res = computedStyle.backgroundColor === computedStyle.color
-
-  document.body.removeChild(elm)
-
-  return res
+  return parseColor(a) === parseColor(b)
 }
