6407670700258088ddfaba837d386a790c58d310
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-07-21 16:37:05 +0900

    fix: support shorthand hex format

--- cypress/utils/compareColor.ts
+++ cypress/utils/compareColor.ts
@@ -1,10 +1,19 @@
-const kRegexColorHex = /^#([0-9a-f]{3}|[0-9a-f]{6})$/
+const kRegexColorHex = /^#([0-9a-f]{6})$/
+const kRegexColorShorthandHex = /^#([0-9a-f]{3})$/
 const kRegexColorRgb = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/
 const kRegexColorRgba = /^rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)$/
 
 const parseColor = (color: string): number => {
   if (kRegexColorHex.test(color)) {
     return parseInt(color.slice(1), 16)
+  } else if (kRegexColorShorthandHex.test(color)) {
+    const [, hex] = color.match(kRegexColorShorthandHex)!
+    const parsed = parseInt(hex, 16)
+    const r = (parsed >> 8) & 0xf
+    const g = (parsed >> 4) & 0xf
+    const b = parsed & 0xf
+    const pat = (r << 20) | (g << 12) | (b << 4)
+    return pat | (pat << 4)
   } else if (kRegexColorRgb.test(color)) {
     const [, r, g, b] = color.match(kRegexColorRgb)!
 
