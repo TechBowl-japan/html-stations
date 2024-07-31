bc0a5213e5e8c892e5928cedefe3de92f4672618
no215tyler <no.215.tyler@gmail.com>
2024-08-01 00:01:10 +0900

    station14: mapメソッドの仕組みを知ろう

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -1,18 +1,34 @@
 function getData() {
-    // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
-    // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
-    // そのため、JavaScriptで連結させて生成することにしました。
-    const test = [
-        { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-        { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
-    ];
-    return test.map(buildFullName)
+  // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
+  // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
+  // そのため、JavaScriptで連結させて生成することにしました。
+  const test = [
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
+  ];
+  const results = test.map(buildFullName);
+  console.log(results);
+  return results;
 }
 
 function buildFullName(data) {
-    // Station14の問題はこちらです。想定する答えをここを書いてください。
-    // 氏名がわかるようにしつつ、半角スペースで繋いでください。
-    // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  data.full_name = data.family_name + " " + data.first_name;
+  return data;
+  // Station14の問題はこちらです。想定する答えをここを書いてください。
+  // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+  // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
 }
 
 // 出力が想定される答え
