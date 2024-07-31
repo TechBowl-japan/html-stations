18c0ca62692779576c15969598ecc7c6b237b1b0
no215tyler <no.215.tyler@gmail.com>
2024-07-31 23:21:34 +0900

    station13: アロー関数を使ってみよう

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,14 +1,12 @@
-data = "global data";
+const data = "global data";
 
 // このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
 // ↓関数の中の処理は変更しないでください
-const returnThisData = function () {
-  return this.data;
-};
+const returnThisData = () => object.data;
 // ↑関数の中の処理は変更しないでください
 
 const object = {
-  data: "object data",
+  data: data,
   func: returnThisData,
 };
 
