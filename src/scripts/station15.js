11ad3b785cb6e0766f1bc271055b05cb0363bcf8
no215tyler <no.215.tyler@gmail.com>
2024-08-01 15:50:11 +0900

    station15: Promiseを使ってみよう

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,31 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      full_name: "大木 優",
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      full_name: "山田 太郎",
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(userList);
+    }, 3000);
+  });
 }
-
