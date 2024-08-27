2365b0c5c77d01800e683d53604ef42d244e88d6
HIROTO ISHIDA <s266021@wakayama-u.ac.jp>
2024-08-27 15:27:45 +0900

    全てコミットします

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,21 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList)
+  return result
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const fullNameList = userList.map(buildFullName);
+      resolve(fullNameList);
+    }, 3000);
+  });
+}
+
+function buildFullName(data) {
+  data.full_name = data.family_name + ' ' + data.first_name
+  return data;
 }
 
